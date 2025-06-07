7dbcabd515dde8bec347da390e3314dcad7bdfd3
martasusenkova <susenkova582@gmail.com>
2025-06-07 14:45:42 +0400

    feat: Add news search feature with input & button, plus safe rendering for articles with missing data (title, description, image)  and update styles

--- script.js
+++ script.js
@@ -2,9 +2,37 @@ const apikey = "5e2303fbd9484c8187e3fa01f9587b12";
 
 const blogContainer = document.getElementById("blog-container");
 
+const searchField = document.getElementById("search-input");
+
+const searchButton = document.getElementById("search-button");
+
 async function fetchRandomNews() {
   try {
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${apikey}`;
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&pageSize=20&apiKey=${apikey}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data.articles;
+  } catch (error) {
+    console.error("Error fetching random news", error);
+    return [];
+  }
+}
+
+searchButton.addEventListener("click", async () => {
+  const query = searchField.value.trim();
+  if (query !== "") {
+    try {
+      const articles = await fetchNewsQuery(query);
+      displayBlogs(articles);
+    } catch (error) {
+      console.log("Error fetching news by query", query);
+    }
+  }
+});
+
+async function fetchNewsQuery(query) {
+  try {
+    const apiUrl = `https://newsapi.org/v2/everything?q=${query}&pageSize=20&apiKey=${apikey}`;
     const response = await fetch(apiUrl);
     const data = await response.json();
     return data.articles;
@@ -20,20 +48,25 @@ function displayBlogs(articles) {
     const blogCard = document.createElement("div");
     blogCard.classList.add("blog-card");
     const img = document.createElement("img");
-    img.src = article.urlToImage;
+    img.src =
+      article.urlToImage || "https://placehold.co/300x200?text=No+Image";
     img.alt = article.title;
     const title = document.createElement("h2");
-    const truncatedTitle =
-      article.title.length > 30
+    const truncatedTitle = article.title
+      ? article.title.length > 30
         ? article.title.slice(0, 30) + "..."
-        : article.title;
+        : article.title
+      : console.log("No title available");
+
     title.textContent = truncatedTitle;
+
     const description = document.createElement("p");
     const truncatedDes =
-      article.description.length > 120
+      article.description && article.description.length > 120
         ? article.description.slice(0, 120) + "..."
-        : article.description;
+        : article.description || "No description";
     description.textContent = truncatedDes;
+
     blogCard.appendChild(img);
     blogCard.appendChild(title);
     blogCard.appendChild(description);
@@ -43,7 +76,7 @@ function displayBlogs(articles) {
     blogContainer.appendChild(blogCard);
   });
 }
-async () => {
+(async () => {
   try {
     const articles = await fetchRandomNews();
     displayBlogs(articles);
@@ -51,4 +84,4 @@ async () => {
   } catch (error) {
     console.error("Error fetching random news", error);
   }
-};
+})();
